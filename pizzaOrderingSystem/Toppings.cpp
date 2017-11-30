c7d0159c093ca2f53c52981d1759d20f64391750
hellpro <brynjarorng@gmail.com>
2017-11-30 09:40:52 +0000

    cleaning up wrongly named files

--- pizzaOrderingSystem/Toppings.cpp
+++ pizzaOrderingSystem/Toppings.cpp
@@ -1,10 +1,27 @@
 #include "Toppings.h"
 
-Toppings::Toppings(string  name, int price)
+Toppings::Toppings(){
+
+}
+void Toppings::convert_string_cstring(string in)
 {
-	this -> price = price;
-	this -> name = name;
-	namegame();
+	int i = 0;
+	while(i < CNAME_SIZE && i < in.length())
+	{
+		cname[i] = in[i];
+		i++;
+	}
+	append_null(i + 1);
+}
+Toppings::Toppings(string name, double price)
+{
+   	this -> price = price;
+	convert_string_cstring(name);
+
+}
+string Toppings::get_name()
+{
+	return cname;
 }
 
 ostream& operator << (ostream& outs, const Toppings& topping)
@@ -14,13 +31,24 @@ ostream& operator << (ostream& outs, const Toppings& topping)
 	return outs;
 }
 
-friend istream& operator >> (istream& ins, Toppings& topping)
+istream& operator >> (istream& ins, Toppings& topping)
 {
-	ins >> topping.name >> topping.price;
+	ins >> topping.cname;
+	ins >> topping.price;
 	return ins;
 }
-
-void Toppings::namegame()
+void Toppings::append_null(int index)
 {
-t	strcpy(name, cname);
+	cname[index - 1] = '\0';
+}
+string Toppings::namegame()
+{
+	string return_string;
+	int i = 0;
+	while(cname[i] != '\0')
+	{
+		return_string[i] = cname[i];
+		i++;
+	}
+	return return_string;
 }
