6b006d37986c83ba8bf369d1873a6c27b43f90da
ViktorSveins <viktorsveins@gmail.com>
2017-11-30 10:12:21 +0000

    files copied for testing

---
+++ pizzaOrderingSystem/30_nov_3layertest/fstreamClass.h
@@ -0,0 +1,24 @@
+#include<iostream>
+#include "Toppings.h"
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class FstreamClass
+{
+public:
+	FstreamClass();
+	~FstreamClass();
+	void print_list();
+	void write_toppings(Toppings topping);
+	void read_toppings();
+	//write_sizes();
+	//read_sizes();
+	//write_pizzas();
+	//read_pizzas();
+
+private:
+	Toppings topping;
+	Toppings* topping_list;
+	int topping_list_size;
+};
