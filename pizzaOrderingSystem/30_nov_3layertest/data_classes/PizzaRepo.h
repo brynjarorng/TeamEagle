81ab2d215c43641dbfabd902ec170d760df238b4
Ivar Marrow <ivara16@ru.is>
2017-11-30 18:17:44 +0000

    added dataclasses for pizza and toppings

---
+++ pizzaOrderingSystem/30_nov_3layertest/data_classes/PizzaRepo.h
@@ -0,0 +1,28 @@
+#ifndef PIZZAREPO_H
+#define PIZZAREPO_H
+
+#include "Pizza.h"
+#include <fstream>
+
+class PizzaRepo {
+	
+public:
+	PizzaRepo();
+	//Currently empty initializer.
+	~PizzaRepo();
+	//Currently empty destructor.
+	void write(Pizza pizza);
+	//Appends pizza to a binary file located in the home directory. 
+	Pizza* read();
+	//Return value is a dynamic array where each element is an occurrence
+	//of a Pizza currently contained in "pizzas.dat" in the home folder.
+	//The allocated memory must be deleted where the pointer is received.
+	int get_list_count() const;
+	//Returns the size of the above list. 
+
+private:
+	Pizza pizza;
+	Pizza* list;  
+	int list_count;
+};
+#endif // PIZZA_REPO
