d09ff63899065b8da12bddab918bafdf427c6fe7
Ivar Marrow <ivara16@ru.is>
2017-11-30 09:33:11 +0000

    new toppings.h and Toppings.cpp

--- pizzaOrderingSystem/29nov/toppings.h
+++ pizzaOrderingSystem/29nov/toppings.h
@@ -1,9 +1,11 @@
+#ifndef TOPPINGS_H
+#define TOPPINGS_H
 #include <string>
 #include<iostream>
 #include <cstring>
+#include <string>
 
 const int   CNAME_SIZE = 20;
-
 
 using namespace std;
 
@@ -12,12 +14,17 @@ class Toppings
 public:
 	Toppings();
 	//Toppings(char cname, int price);
-	void namegame();
+	Toppings(string name, double price);
+	//
+	string namegame();
+	//Makes this->cname a copy of name[].
 	friend ostream& operator << (ostream& outs, const Toppings& topping);
 	friend istream& operator >> (istream& ins, Toppings& topping);
+	string get_name();  
 private:
-	//string name;
 	char cname[CNAME_SIZE];
 	double price;
-	
+	void append_null(int index);
+	void convert_string_cstring(string in);
 };
+#endif //TOPPINGS_H
