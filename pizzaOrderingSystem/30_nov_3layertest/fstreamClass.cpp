e7eb2734e8b94b68019ef38ab41a84318b4ac57a
ViktorSveins <viktorsveins@gmail.com>
2017-11-30 11:02:14 +0000

    ready for first test of all 3 layers including ui

--- pizzaOrderingSystem/30_nov_3layertest/fstreamClass.cpp
+++ pizzaOrderingSystem/30_nov_3layertest/fstreamClass.cpp
@@ -1,11 +1,11 @@
 #include "fstreamClass.h"
-void FstreamClass::print_list()
+/*void FstreamClass::print_list()
 {
 	for (int i = 0; i < topping_list_size; i++) {
 		cout << topping_list[i] << endl;
 	}
 }
-
+*/
 
 FstreamClass::FstreamClass()
 {
@@ -13,7 +13,7 @@ FstreamClass::FstreamClass()
 }
 FstreamClass::~FstreamClass()
 {
-	delete[] topping_list;
+ //
 }
 void FstreamClass ::write_toppings(Toppings topping)
 {
@@ -21,12 +21,10 @@ void FstreamClass ::write_toppings(Toppings topping)
 	fout.write((char*)(&topping), sizeof(Toppings));
 	fout.close();
 }
-void FstreamClass::read_toppings()
+Toppings* FstreamClass::read_toppings()
 {
 
 	ifstream fin("toppings.dat", ios::binary);
-
-
 
 	if (fin.is_open()) {
 		fin.seekg(0, fin.end);
@@ -41,9 +39,9 @@ void FstreamClass::read_toppings()
 		fin.read((char*)(topping_list), sizeof(Toppings)*record_count);
 
 		fin.close();
-
-
 	}
+
+	return topping_list;
 
 //	return &topping_list;
 }
