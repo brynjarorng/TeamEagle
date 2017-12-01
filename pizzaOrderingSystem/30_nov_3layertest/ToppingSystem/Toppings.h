b5c172bdf85a151e96f8e9e317ae4db6ff796547
Ivar Marrow <ivara16@ru.is>
2017-12-01 10:51:35 +0000

    Working datasystem for Topping

---
+++ pizzaOrderingSystem/30_nov_3layertest/ToppingSystem/Toppings.h
@@ -0,0 +1,37 @@
+#ifndef TOPPINGS_H
+#define TOPPINGS_H
+#include <string>
+#include<iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+class Toppings
+{
+public:
+	Toppings();
+	//Initializes cname[0] = '\0' and price to 0.0. 
+	Toppings(string name, double price);
+	//Sets this ->price to price, and converts name to a corresponding
+	//charachter array.
+	//string namegame();
+	
+	friend ostream& operator << (ostream& outs, const Toppings& topping);
+	//Outstreams name of topping followed by a whitespace and then the price
+	//of the topping.
+	friend istream& operator >> (istream& ins, Toppings& topping);
+	//The order of instream is the following:
+	//name then price.
+	string get_name();
+	//Returns the name of topping
+	double get_price();
+private:
+	static const int CNAME_SIZE = 21;
+	char cname[CNAME_SIZE];	//Name of topping.
+	double price;
+	void append_null(int index);
+	//Postcondtion: cname[index - 1] is now '\0'
+	void convert_string_cstring(string in);
+};
+#endif //TOPPINGS_H
---
+++ pizzaOrderingSystem/30_nov_3layertest/ToppingSystem/ToppingsHandler.h
@@ -0,0 +1,33 @@
+#ifndef TOPPINGHANDLER_H
+#define TOPPINGHANDLER_H
+
+#include "Toppings.h"
+#include "ToppingsRepo.h"
+#include <iostream>
+using namespace std;
+
+class ToppingsHandler
+{
+    public:
+        ToppingsHandler();
+		//Initializes a private pointer of type Toppings to
+		//point to an empty array of Toppings.
+		~ToppingsHandler();
+        void create_toppings();
+		//Prompts the user to input the name and price of the toppings,
+		//and appends to a list saved in the home folder.
+        void print_toppings();
+		//Prints the list of Toppings in the same order as the current list.
+        void operator =(Toppings* copy);
+		//First delete memory assigned to the private pointer of type Toppings,
+		//and then make it equal to copy.
+    private:
+        ToppingsRepo repo;
+		//Access to the data layer is through ToppingsRepo.
+		Toppings* toppings_list;
+		//The list of toppings that recevies information from the ToppingsRepo.
+		int toppings_list_count;
+		//Size of the above list.
+};
+
+#endif // TOPPINGHANDLER_H
