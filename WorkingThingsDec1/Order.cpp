2bdca54e070df4eab0ebcbbb196eb2272287e7cf
Ivar Marrow <ivara16@ru.is>
2017-12-02 18:14:33 +0000

    Order added for the first time

---
+++ WorkingThingsDec1/Order.cpp
@@ -0,0 +1,115 @@
+#ifndef ORDER_H_INCLUDED
+#define ORDER_H_INCLUDED
+
+#include "Order.h"
+
+Order::Order() {
+	this ->order_ready = false;
+	this ->order_delivered = false;
+	this ->pizzas_in_order_count = 0;
+}
+Pizza* Order::get_order() {
+
+	return this->pizzas_in_order;
+}
+void Order::set_delivered() {
+
+	this ->order_delivered = true;
+}
+
+bool Order::delivered() {
+
+	return this ->order_delivered;
+}
+int Order::get_order_count() {
+	
+	return this ->pizzas_in_order_count;
+}
+
+ostream& operator <<(ostream& outs, Order& order) {
+
+	for (int i = 0; i < order.pizzas_in_order_count; i++) {
+
+		outs << "Order nr #" << order.order_number << endl;
+		
+		for (int i = 0; i < order.pizzas_in_order_count; i++) {
+
+			outs << order.pizzas_in_order[i] << endl;
+		}
+	}
+	return outs;
+}
+
+istream& operator >>(istream& ins, Order& order) {
+
+	Pizza temp;
+	ins >> temp;
+	int index = order.pizzas_in_order_count;
+	order.pizzas_in_order[index] = temp;
+	order.pizzas_in_order_count++;
+	
+	return ins;
+}
+void Order::all_pizzas_ready() {
+	
+	bool temp = true; //Temporary until pizzahandler is ready.
+	int ready_count = 0;
+	for (int i = 0; i < pizzas_in_order_count; i++) {
+		if (temp) {
+			//Remember ->pizzas_in_order[i].get_status() == read
+			ready_count++;
+		}
+	}
+	if (ready_count == pizzas_in_order_count) {
+
+		this ->order_ready = true;
+	}
+}
+bool Order::ready() {
+
+	all_pizzas_ready();
+	return this ->order_ready;
+}
+void Order::set_order_number(int number) {
+
+	this ->order_number = number;
+}
+void Order::add_pizza(Pizza pizza) {
+	if(pizzas_in_order_count <= MAX_ORDER_SIZE) {
+
+		this ->pizzas_in_order[this ->pizzas_in_order_count] = pizza;
+		this ->pizzas_in_order_count++;
+	}
+			
+	else {
+		cout << "Order has reached maximum size!" << endl;	
+	}
+}
+void Order::add_pizza_to_order(PizzaType pizzatype) {
+
+	PizzaHandler pizzahandler;
+	Pizza temp_pizza;
+	
+	if (pizzatype == menu_pizza) {
+
+		string pizza_name;
+		cin >> pizza_name;
+
+		bool on_menu = pizzahandler.validate_pizza(pizza_name);
+		if (on_menu) {
+			temp_pizza = pizzahandler.get_menu_pizza(pizza_name);
+			add_pizza(temp_pizza);
+		}
+		else {
+			cout << "Pizza not on menu! " << endl;
+		}
+	}
+	else if (pizzatype == special_pizza) {
+		temp_pizza = pizzahandler.create_special_pizza();
+		add_pizza(temp_pizza);
+	}
+	else {
+		cout << "---(SYSTEM)NOT A PIZZATYPE---"  << endl;
+	}
+}
+#endif
---
+++ WorkingThingsDec1/Order.h
@@ -0,0 +1,54 @@
+#ifndef ORDER_H
+#define ORDER_H
+#include "PizzaHandler.h"
+#include "PizzaType.h"
+class Order {
+	
+public:
+	
+	Order();
+	//Initializes order_ready and order_delivered to false,
+	//also the pizza count in the order is set to 0.
+	Pizza* get_order();
+	//Returns an array of pizzas in the order.
+	void set_delivered();
+	//Marks the order as delivered.
+	bool delivered();
+	//The return value is TRUE if the pizza is marked delivered
+	//FALSE otherwise.
+	friend ostream& operator <<(ostream& outs, Order& order);
+	//Outstream will be each pizza in order followed by information
+	//about pizza status: pizza in oven (T/F) and pizza ready (T/F).
+	friend istream& operator >>(istream& ins, Order& order);
+	//Instream is one occurrence of pizza wich will be the next element
+	//in the private member array containing the pizzas in the order.
+	void add_pizza_to_order(PizzaType pizzatype);
+	//Add pizza to order, if Pizzatype = menu_pizza the a pizza of type
+	//menu_pizza is added to the order, if PizzaType = special_pizza
+	//then a pizza of type special pizza is added. If the Pizzatype is
+	//neither of these, an error message is printed.
+	void set_order_number(int number);
+	//Sets the order_number of this instance of order equal to number
+	bool ready();
+	//Return value is true if all pizzas in orders are ready.
+	//False otherwise.
+	int get_order_count();
+	//Returns the number of pizzas in the order.
+private:
+	static const int MAX_ORDER_SIZE = 100;
+
+	bool order_ready;
+    //True if all pizzas are ready in order.
+	bool order_delivered;
+    //True if pizza is payed.
+	Pizza pizzas_in_order[MAX_ORDER_SIZE];
+	int pizzas_in_order_count;//Size of above array.
+	int order_number;
+    //Number of this instance of order.
+	void all_pizzas_ready();
+    //Returns true if all pizzas are marked ready.
+	void add_pizza(Pizza pizza);
+	//If pizzas_in_order_count <= MAX_ORDER_SIZE, pizza is added
+	//to order, else an error message is printed to the screen.
+};
+#endif //ORDER
