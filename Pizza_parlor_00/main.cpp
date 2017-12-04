8b7c797bfcb095e440c83aaa03be6b63e9834173
hellpro <brynjarorng@gmail.com>
2017-12-04 10:33:39 +0000

    Added UI, not implemented yet

--- Pizza_parlor_00/main.cpp
+++ Pizza_parlor_00/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include "OrderHandler.h"
+#include <UI.h>
 using namespace std;
 
 int main()
 {
+/*
+    UI ui;
+
+    ui.print_login_selector(0);
+*/
+
+
     OrderHandler orderhandler;
 
-    for (int i = 0; i < 2; i++)
+    //for (int i = 0; i < 2; i++)
         orderhandler.add_pizza_to_order(menu_pizza);
 
     orderhandler.print_current_list();
