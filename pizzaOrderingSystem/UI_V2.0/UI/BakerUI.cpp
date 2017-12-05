0a51a7eace517f8b1188b1fe3ed1dc34d3d07509
hellpro <brynjarorng@gmail.com>
2017-12-05 15:40:02 +0000

    Fixed all recursive calls to be non-recursive in order to avoid stack overflow

--- pizzaOrderingSystem/UI_V2.0/UI/BakerUI.cpp
+++ pizzaOrderingSystem/UI_V2.0/UI/BakerUI.cpp
@@ -5,32 +5,9 @@ BakerUI::BakerUI() {
 
 }
 
-int BakerUI::baker_menu() {
+bool BakerUI::baker_menu(bool run, bool& continueRun) {
     system("CLS");
     cout << "----Baker menu----" << endl;
     system("Pause");
     return 0;
 }
-
-void BakerUI::terminate() {
-    system("CLS");
-    char cont;
-
-    cout << "Are you sure you want to quit (y/n)? ";
-        cin >> cont;
-        cont = lower(cont);
-
-    if(cont == 'y'){
-        cout << "Good Bye!" << endl;
-        exit(0);
-    }
-    baker_menu();
-    //Double check if user wants to quit, else runs the
-    //manager UI again
-}
-
-char BakerUI::lower(char instring) {
-    instring = tolower(instring);
-    return instring;
-}
-
--- pizzaOrderingSystem/UI_V2.0/UI/ManagerUI.h
+++ pizzaOrderingSystem/UI_V2.0/UI/ManagerUI.h
@@ -3,18 +3,28 @@
 
 #include <stdlib.h>
 #include <iostream>
+#include "InvalidInputException.h"
+
 using namespace std;
 
 class ManagerUI {
     public:
         ManagerUI();
-        int manager_menu(bool run);
-        void info_menu(bool run);
-        void topping_menu(bool run);
-        void size_menu(bool run);
-        void menu_pizza_menu(bool run);
+        bool manager_menu(bool run, bool& continueRun);
+        //Displays the main manager menu and asks for input
+        bool info_menu(bool run, bool& continueRun);
+        //Displays the information menu (Toppings info, pizza info etc.)
+        bool topping_menu(bool run, bool& continueRun);
+        //Displays the menu for adding and removing toppings
+        bool size_menu(bool run, bool& continueRun);
+        //Displays the menu for adding and removing pizza sizes
+        bool menu_pizza_menu(bool run, bool& continueRun);
+        //Displays the menu for adding and removing pizzas from the menu
         char lower(char instr);
+        //takes in a character and makes it
+        //lower case (A -> a)
         void terminate();
+        //stop the program
 
     private:
         char select;
