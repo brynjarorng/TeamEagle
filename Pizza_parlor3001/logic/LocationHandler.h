75e99ec459fb42357470a95aa272c86b2846f9da
ViktorSveins <viktorsveins@gmail.com>
2017-12-11 11:55:31 +0000

    locationrepo and handler added

---
+++ Pizza_parlor3001/logic/LocationHandler.h
@@ -0,0 +1,19 @@
+/*#ifndef LOCATIONHANDLER_H
+#define LOCATIONHANDLER_H
+
+
+class LocationHandler
+{
+    public:
+        LocationHandler();
+        virtual ~LocationHandler();
+
+    protected:
+
+    private:
+        LocationRepo locrepo;
+        location_name_length = 50;
+};
+
+#endif // LOCATIONHANDLER_H
+*/
