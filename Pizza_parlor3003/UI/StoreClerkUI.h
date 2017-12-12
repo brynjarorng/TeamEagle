add1939100f8bfc2ce28fac67374a5d4422732c9
hellpro <brynjarorng@gmail.com>
2017-12-12 13:54:51 +0000

    Added sizes and a basic price calculator

---
+++ Pizza_parlor3003/UI/StoreClerkUI.h
@@ -0,0 +1,25 @@
+#ifndef STORECLERK_H
+#define STORECLERK_H
+
+#include <stdlib.h>
+#include <iostream>
+#include "InvalidInputException.h"
+#include "print_functions.h"
+using namespace std;
+
+class StoreClerkUI {
+    public:
+        StoreClerkUI();
+        bool store_menu(bool run, bool& continueRun);
+        //Displays the store clerk menu
+        char lower(char instr);
+        //takes in a character and makes it
+        //lower case (A -> a)
+        void terminate();
+        //stop the program
+
+    private:
+        char select;
+};
+
+#endif // STORECLERK_H
---
+++ Pizza_parlor3003/exceptions/InvalidDoubleException.h
@@ -0,0 +1,17 @@
+#ifndef INVALIDDOUBLEEXCEPTION_H
+#define INVALIDDOUBLEEXCEPTION_H
+
+#include <string>
+
+using namespace std;
+
+class InvalidDoubleException
+{
+    public:
+        InvalidDoubleException();
+        string get_err();
+
+    private:
+};
+
+#endif // INVALIDDOUBLEEXCEPTION_H
