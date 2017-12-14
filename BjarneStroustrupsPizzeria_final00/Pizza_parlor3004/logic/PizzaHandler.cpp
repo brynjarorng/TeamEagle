c28f0f013cb4a572ce0d293f58043d7a56592123
ViktorSveins <viktorsveins@gmail.com>
2017-12-14 19:07:19 +0000

    bottom validation fix

--- BjarneStroustrupsPizzeria_final00/Pizza_parlor3004/logic/PizzaHandler.cpp
+++ BjarneStroustrupsPizzeria_final00/Pizza_parlor3004/logic/PizzaHandler.cpp
@@ -172,12 +172,12 @@ vector<Pizza> PizzaHandler::get_pizza_list(){
     return pizzas;
  }
 
-void PizzaHandler::add_bottom_to_pizza(Pizza& pizza, int input) {
+void PizzaHandler::add_bottom_to_pizza(Pizza& pizza, int index) {
     vector<PizzaBottom> bottom_vector;
     PizzaBottomHandler bottom_handler;
-    if(input >= 0 && input < bottom_vector.size()) {
+    if(index >= 0 && index < bottom_vector.size()) {
         bottom_vector = bottom_handler.get_size_list();
-        pizza.set_bottom(bottom_vector.at(input-1));
+        pizza.set_bottom(bottom_vector.at(index));
     }
     else {
         throw InvalidSize();
